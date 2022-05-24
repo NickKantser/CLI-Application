bf2ff6ab117202e52feac2fd02e787bbce2a3658
NickKantser <kolia.falka@gmail.com>
2022-05-24 15:16:34 +0200

    create File model

--- server_django/files/models.py
+++ server_django/files/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class File(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    size = models.PositiveIntegerField()
+    mimetype = models.CharField(max_length=30)
