ab9ce34254efe6999f49382dab0b9863a39e5f75
NickKantser <kolia.falka@gmail.com>
2022-05-27 13:49:32 +0200

    add File model

--- server_django/files/models.py
+++ server_django/files/models.py
@@ -5,3 +5,4 @@ class File(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     size = models.PositiveIntegerField()
     mimetype = models.CharField(max_length=30)
+    file = models.FileField(upload_to='media/%Y/%m/%d/')
