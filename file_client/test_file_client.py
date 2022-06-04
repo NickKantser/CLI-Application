1864d2297cda399bc9f3c52e4d4740ff26fe0787
NickKantser <kolia.falka@gmail.com>
2022-06-04 17:25:32 +0200

    edit tests of file-client

--- file_client/test_file_client.py
+++ file_client/test_file_client.py
@@ -27,13 +27,13 @@ class TestFileClient(unittest.TestCase):
     def test_default_of_options(self, mock_req):
         result = self.runner.invoke(file_client.cli, ['stat', '2'])
 
-        mock_req.get.assert_called_once_with('http://localhost:50051/file/2/stat')
+        mock_req.get.assert_called_once_with('http://localhost:8000/file/2/stat')
         self.assertEqual(result.exit_code, 0)
 
     # Tests that http:// is in the address and if not it's added
     @patch('file_client.requests')
     def test_http_in_address(self, mock_req):
-        result = self.runner.invoke(file_client.cli, ['--grpc-server=somelink', 'read', '1'])
+        result = self.runner.invoke(file_client.cli, ['--django-server=somelink', 'read', '1'])
 
         mock_req.get.assert_called_once_with('http://somelink/file/1/read')
         self.assertEqual(result.exit_code, 0)
@@ -50,7 +50,7 @@ class TestFileClient(unittest.TestCase):
             with open('text.txt', 'rb') as f:
                 file_content = f.read()
 
-        mock_req.get.assert_called_once_with('http://localhost:50051/file/1/stat')
+        mock_req.get.assert_called_once_with('http://localhost:8000/file/1/stat')
         self.assertEqual(file_content, mock_req.get.return_value.content)
         self.assertEqual(result.exit_code, 0)
 
@@ -63,7 +63,7 @@ class TestFileClient(unittest.TestCase):
 
         result = self.runner.invoke(file_client.cli, ['stat', '1'])
 
-        mock_req.get.assert_called_once_with('http://localhost:50051/file/1/stat')
+        mock_req.get.assert_called_once_with('http://localhost:8000/file/1/stat')
         self.assertEqual(result.stdout_bytes, mock_req.get.return_value.content)
         self.assertEqual(result.exit_code, 0)
 
@@ -81,7 +81,7 @@ class TestFileClient(unittest.TestCase):
             with open('text.txt', 'rb') as f:
                 file_content = f.read()
 
-        mock_req.get.assert_called_once_with('http://localhost:50051/file/1/stat')
+        mock_req.get.assert_called_once_with('http://localhost:8000/file/1/stat')
         self.assertEqual(expected_output, file_content)
         self.assertEqual(result.exit_code, 0)
 
@@ -95,14 +95,14 @@ class TestFileClient(unittest.TestCase):
 
         result = self.runner.invoke(file_client.cli, ['stat', '1'])
 
-        mock_req.get.assert_called_once_with('http://localhost:50051/file/1/stat')
+        mock_req.get.assert_called_once_with('http://localhost:8000/file/1/stat')
         self.assertEqual(expected_output, result.stdout_bytes)
         self.assertEqual(result.exit_code, 0)
 
     # Tests case of invalid address -> file output
     def test_invalid_address_with_file_output(self):
-        url = 'http://localhost:50051'
-        backend = 'grpc'
+        url = 'http://localhost:8000'
+        backend = 'django'
 
         expected_output = f'Can\'t connect to the {backend} server at address {url}\n'
         expected_output = expected_output.encode()
@@ -120,8 +120,8 @@ class TestFileClient(unittest.TestCase):
 
     # Tests case of invalid address -> stdout
     def test_invalid_address_with_stdout(self):
-        url = 'http://localhost:50051'
-        backend = 'grpc'
+        url = 'http://localhost:8000'
+        backend = 'django'
 
         expected_output = f'Can\'t connect to the {backend} server at address {url}\n'
         expected_output = expected_output.encode()
@@ -131,30 +131,30 @@ class TestFileClient(unittest.TestCase):
         self.assertEqual(expected_output, result.stdout_bytes)
         self.assertEqual(result.exit_code, 0)
 
-    # Test that if --backend=rest, then request address corresponds to the --base-url option
+    # Test that if --backend=django, then request address corresponds to the --django-server option
     @patch('file_client.requests')
     def test_backend_rest_and_base_url(self, mock_req):
         mock_req.get.return_value.status_code = 200
         mock_req.get.return_value.content = b"{'filename': 'some.txt'}"
 
 
-        result = self.runner.invoke(file_client.cli, ['--backend=rest',
-                                                      '--grpc-server=http://localhost:14000/',
-                                                      '--base-url=http://localhost:15000/',
+        result = self.runner.invoke(file_client.cli, ['--backend=django',
+                                                      '--flask-server=http://localhost:14000/',
+                                                      '--django-server=http://localhost:15000/',
                                                       'read', '2'])
 
         mock_req.get.assert_called_once_with('http://localhost:15000/file/2/read')
         self.assertEqual(result.exit_code, 0)
 
-    # Test that if --backend=grpc, then request address corresponds to the --grpc-server option
+    # Test that if --backend=flask, then request address corresponds to the --flask-server option
     @patch('file_client.requests')
     def test_backend_grpc_and_grpc_server(self, mock_req):
         mock_req.get.return_value.status_code = 200
         mock_req.get.return_value.content = b"{'filename': 'some.txt'}"
 
-        result = self.runner.invoke(file_client.cli, ['--backend=grpc',
-                                                      '--grpc-server=http://localhost:14000/',
-                                                      '--base-url=http://localhost:15000/',
+        result = self.runner.invoke(file_client.cli, ['--backend=flask',
+                                                      '--flask-server=http://localhost:14000/',
+                                                      '--django-server=http://localhost:15000/',
                                                       'read', '2'])
 
         mock_req.get.assert_called_once_with('http://localhost:14000/file/2/read')
